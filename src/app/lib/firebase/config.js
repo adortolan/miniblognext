5802bd3fdb1ae3d6eed72b812593df114c7fda1f
adortolan <adortolan@gmail.com>
2025-05-15 10:16:11 -0300

    feat: Finalizando as variaveis de ambiente

--- src/app/lib/firebase/config.js
+++ src/app/lib/firebase/config.js
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  //apiKey: "AIzaSyDz1iutOPlZRvoa-HMx1lnFwEGe_hlmtPI",
   apiKey: process.env.apiKey,
-  authDomain: "miniblognext.firebaseapp.com",
-  projectId: "miniblognext",
-  storageBucket: "miniblognext.firebasestorage.app",
-  messagingSenderId: "775553570116",
-  appId: "1:775553570116:web:28f007a385d1f6b443b311",
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
